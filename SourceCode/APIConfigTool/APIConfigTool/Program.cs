66a32eef0aafd6f4e13a13c41f66990d3028c650
newsike <newlift.sike@gmail.com>
2016-11-15 12:59:42 +0800

    new code
    new code

--- SourceCode/APIConfigTool/APIConfigTool/Program.cs
+++ SourceCode/APIConfigTool/APIConfigTool/Program.cs
@@ -23,6 +23,11 @@ namespace APIConfigTool
                 command = Console.ReadLine();
                 if (command == "exit")
                 {
+                    string isSave = "yes";
+                    Console.WriteLine("Do you want to save the config file before you quit?(type yes):");
+                    isSave = Console.ReadLine();
+                    if (isSave == "yes")
+                        _objectConfig.DoSave();
                     Console.WriteLine("Goodbye.");
                     Environment.Exit(0);
                 }
@@ -52,21 +57,30 @@ namespace APIConfigTool
                     else
                         Console.WriteLine(">| faild to open the file.");
                 }
+                else if (command == "DES mode On")
+                {
+                    string DESKey = "";
+                    Console.WriteLine(">| DES Key Value : ");
+                    DESKey = Console.ReadLine();
+                    _objectConfig.SwitchToDESModeON(DESKey);
+                    Console.WriteLine(">| DES Mode On. ");
+                }
+                else if(command == "DES mode Off")
+                {
+                    Console.WriteLine(">| DES Mode Off. ");
+                }
                 else if (command == "create new session")
                 {
                     string sessionName = "";
                     string sessionValue = "";
-                    string sessionKey = "";
                     while (sessionName == "")
                     {
                         Console.WriteLine(">| session name : ");
                         sessionName = Console.ReadLine();                        
                     }
                     Console.WriteLine(">| session value : ");
-                    sessionValue = Console.ReadLine();
-                    Console.WriteLine(">| session key : ");
-                    sessionKey = Console.ReadLine();
-                    _objectConfig.CreateNewSession(sessionName, sessionValue, false, sessionKey);
+                    sessionValue = Console.ReadLine();                    
+                    _objectConfig.CreateNewSession(sessionName, sessionValue);
                     Console.WriteLine(">| created new session.");
                 }
                 else if (command == "remove session")
@@ -85,8 +99,7 @@ namespace APIConfigTool
                     string sessionName = "";
                     string attrName = "";
                     string attrValue = "";
-                    string attrKey = "";
-                    while (sessionName == "")
+                     while (sessionName == "")
                     {
                         Console.WriteLine(">| session name : ");
                         sessionName = Console.ReadLine();
@@ -97,10 +110,8 @@ namespace APIConfigTool
                         attrName = Console.ReadLine();
                     }
                     Console.WriteLine(">| attr value : ");
-                    attrValue = Console.ReadLine();
-                    Console.WriteLine(">| attr key : ");
-                    attrKey = Console.ReadLine();
-                    if(_objectConfig.SetSessionAttr(sessionName, attrName, attrValue, false, attrKey))
+                    attrValue = Console.ReadLine();                   
+                    if(_objectConfig.SetSessionAttr(sessionName, attrName, attrValue))
                         Console.WriteLine(">| setted new attr value.");
                     else
                         Console.WriteLine(">| faild to set attr value.");
@@ -141,8 +152,7 @@ namespace APIConfigTool
                     while(true)
                     {
                         string itemName = "" ;
-                        string itemValue = "";
-                        string itemKey = "";                        
+                        string itemValue = "";                        
                         Console.WriteLine(">| new item name : ");
                         itemName = Console.ReadLine();
                         if(itemName == "exit create item")
@@ -150,10 +160,8 @@ namespace APIConfigTool
                         if (string.IsNullOrEmpty(itemName))
                             continue;
                         Console.WriteLine(">| new item value : ");
-                        itemName = Console.ReadLine();
-                        Console.WriteLine(">| new item key : ");
-                        itemKey = Console.ReadLine();
-                        _objectConfig.CreateItem(activeSessionNode, itemName, itemValue, false, itemKey);
+                        itemName = Console.ReadLine();                       
+                        _objectConfig.CreateItem(activeSessionNode, itemName, itemValue);
                         Console.WriteLine(">| created new item. ");
                     }                  
                       
@@ -161,6 +169,7 @@ namespace APIConfigTool
                 else if (command == "save file")
                 {
                     _objectConfig.DoSave();
+                    Console.WriteLine(">| saved file. ");
                 }
                 else if (command == "set item attr value")
                 {
@@ -179,22 +188,46 @@ namespace APIConfigTool
                     string itemName = "";
                     string itemAttrName = "";
                     string itemAttrValue = "";
-                    string itemAttrKey = "";
                     Console.WriteLine(">| item name : ");
                     itemName = Console.ReadLine();
                     Console.WriteLine(">| item attr name : ");
                     itemAttrName = Console.ReadLine();
                     Console.WriteLine(">| item attr value : ");
-                    itemAttrValue = Console.ReadLine();
-                    Console.WriteLine(">| item attr key : ");
-                    itemAttrKey = Console.ReadLine();
+                    itemAttrValue = Console.ReadLine();                   
                     XmlNode activeItemNode = _objectConfig.GetItemNode(activeSessionNode,itemName);
-                    if(_objectConfig.SetItemAttr(activeItemNode, itemAttrName, itemAttrValue, false, itemAttrKey))
+                    if(_objectConfig.SetItemAttr(activeItemNode, itemAttrName, itemAttrValue))
                         Console.WriteLine(">| setted new item attr value.");
                     else
                         Console.WriteLine(">| faild to set item attr value.");
 
                 }
+                else if(command == "get session value")
+                {
+                    string sessionName = "";
+                    while (sessionName == "")
+                    {
+                        Console.WriteLine(">| session name : ");
+                        sessionName = Console.ReadLine();
+                    }                    
+                    Console.WriteLine(">| session value : " + _objectConfig.GetSessionValue(sessionName));
+                }
+                else if(command == "get session attr value")
+                {
+                    string sessionName = "";
+                    string attrName = "";
+                    while (sessionName == "")
+                    {
+                        Console.WriteLine(">| session name : ");
+                        sessionName = Console.ReadLine();
+                    }
+                    while (attrName == "")
+                    {
+                        Console.WriteLine(">| attr name : ");
+                        attrName = Console.ReadLine();
+                    }                  
+                    XmlNode activeSessionNode = _objectConfig.GetSessionNode(sessionName);
+                    Console.WriteLine(">| attr : @" + attrName + " value : " + _objectConfig.GetAttrValue(activeSessionNode, attrName));
+                }                     
                 else
                     Console.WriteLine("<<< Invalidate Command");
             }
