ad583df0e587362b151c13a2473d800ecbf73680
Sike.Chen <sike.chen@outlook.com>
2017-02-15 01:10:17 +0800

    changed

--- SourceCode/iKCoderDU/iKCoderDU/ImportBinData.Designer.cs
+++ SourceCode/iKCoderDU/iKCoderDU/ImportBinData.Designer.cs
@@ -254,6 +254,7 @@
             this.button3.TabIndex = 33;
             this.button3.Text = "导入数据";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // openFileDialog1
             // 
