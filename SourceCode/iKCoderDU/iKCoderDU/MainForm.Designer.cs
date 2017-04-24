d6bbad149726b0c7651926cb4c917edd09825d5e
Sike.Chen <sike.chen@outlook.com>
2017-04-24 23:29:58 +0800

    new code

--- SourceCode/iKCoderDU/iKCoderDU/MainForm.Designer.cs
+++ SourceCode/iKCoderDU/iKCoderDU/MainForm.Designer.cs
@@ -1354,7 +1354,7 @@
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackgroundImage = global::iKCoderDU.Properties.Resources.mainform_fw;
-            this.ClientSize = new System.Drawing.Size(1200, 772);
+            this.ClientSize = new System.Drawing.Size(1200, 900);
             this.Controls.Add(this.lb_serverconnected);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button2);
